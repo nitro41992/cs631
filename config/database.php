92afcf1013bdb22c9b7fa993786715cc62e785f2
nitro41992 <nick.k@iton.net>
2019-04-29 20:56:22 -0400

    Task: reverted config/database.php.

--- config/database.php
+++ config/database.php
@@ -2,13 +2,6 @@
 
 use Illuminate\Support\Str;
 
-$url = parse_url(getenv("DATABASE_URL"));
-
-$host = $url["host"];
-$username = $url["user"];
-$password = $url["pass"];
-$database = substr($url["path"], 1);
-
 return [
 
     /*
@@ -70,11 +63,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $host, //env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
-            'database' => $database, //env('DB_DATABASE', 'forge'),
-            'username' => $username, //env('DB_USERNAME', 'forge'),
-            'password' => $password, //env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
